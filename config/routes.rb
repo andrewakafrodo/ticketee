1eeb8ca050c3aebff9e36f2260ae75addc192d4a
andrewakafrodo <andrewrkowalczyk@gmail.com>
2014-06-20 11:52:21 +1000

    added admin namespace users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 EdgeTicketee::Application.routes.draw do
+
   devise_for :users
 
   root :to => "projects#index"
   resources :projects do
     resources :tickets
+  end
+  
+  namespace :admin do
+    resources :users
   end
 end
